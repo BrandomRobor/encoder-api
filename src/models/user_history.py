1d27ae1512064a840e994b7882796b2495e7efd8
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-22 21:23:41 -0700

    Added collection name to model

--- src/models/user_history.py
+++ src/models/user_history.py
@@ -2,5 +2,9 @@ from models.history import HistoryEntry
 from db import db
 
 class UserHistoryEntry(db.Document):
+    meta = {
+        "collection": "history"
+    }
+
     user_id = db.ObjectIdField()
     history = db.ListField(HistoryEntry())
