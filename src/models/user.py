1243fdaff424983c941e4e9ea89f4d1c17fdf48a
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-22 21:38:02 -0700

    Created User model

---
+++ src/models/user.py
@@ -0,0 +1,10 @@
+from db import db
+
+class UserModel(db.Document):
+    meta = {
+        "collection": "users"
+    }
+
+    username = db.StringField(required=True, unique=True, max_length=50)
+    password = db.StringField(required=True)
+    email = db.EmailField(required=True, unique=True)
