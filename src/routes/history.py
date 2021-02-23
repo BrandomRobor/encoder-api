e4f8a60ae206d8691d5da59141bddcdd747fb05a
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-22 23:07:28 -0700

    Added authentication and error handling to history route

--- src/routes/history.py
+++ src/routes/history.py
@@ -1,9 +1,18 @@
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.user_history import UserHistoryEntry
-from flask import Blueprint
+from flask import Blueprint, Response
+from mongoengine import DoesNotExist
+from bson import ObjectId
 
 history = Blueprint(__name__, __name__)
 
 @history.route("/api/history", methods=["GET"])
+@jwt_required()
 def get_history():
-    return UserHistoryEntry.objects().to_json()
+    try:
+        user_history = UserHistoryEntry.objects.get(user_id=get_jwt_identity())
+    except DoesNotExist:
+        return Response(status=404)
+    else:
+        return user_history.to_json()
     
