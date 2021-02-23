ff0cad5090cb3b769576ea0a9eb2de9103c8ad25
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-22 22:53:43 -0700

    Added JWT manager to app and route for login

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
+from flask_jwt_extended import JWTManager
 from routes.history import history
 from routes.users import users
 from db import initialize_db
+
 from flask import Flask
 import os
 
 app = Flask(__name__)
 app.config["MONGODB_HOST"] = os.environ.get("MONGODB_HOST")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 initialize_db(app)
+jwt = JWTManager(app)
 
 app.register_blueprint(history)
 app.register_blueprint(users)
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,4 +1,5 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
 from flask import Blueprint, request, Response
 from models.user import UserModel
 
@@ -16,3 +17,16 @@ def register_user():
         return Response(status=500)
     else:
         return Response(status=201)
+
+
+@users.route("/api/user", methods=["POST"])
+def user_login():
+    body = request.get_json()
+
+    try:
+        user_data = UserModel.objects.get(username=body["username"])
+    except:
+        return Response(status=500)
+    else:
+        if check_password_hash(user_data["password"], body["password"]):
+            return create_access_token(str(user_data.id))
