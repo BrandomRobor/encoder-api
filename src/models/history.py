c8c51f75e8b2e87203aa34e9a04712e39afcf0e5
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-23 16:58:10 -0700

    Added id property to history model

--- src/models/history.py
+++ src/models/history.py
@@ -1,6 +1,10 @@
-from db import db
 from datetime import datetime
+from bson import ObjectId
+from db import db
+
 
 class HistoryEntry(db.EmbeddedDocument):
+    entry_id = db.ObjectIdField(
+        required=True, default=ObjectId, unique=True, primary_key=True)
     filename = db.StringField(required=True)
-    date = db.DateTimeField(required=True,default=datetime.utcnow)
+    date = db.DateTimeField(required=True, default=datetime.utcnow)
