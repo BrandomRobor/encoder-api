02ed7385e3ba8f476e6e7557ccdf061286c2d183
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-22 23:33:01 -0700

    Added default field to history model

--- src/models/history.py
+++ src/models/history.py
@@ -1,5 +1,6 @@
 from db import db
+from datetime import datetime
 
 class HistoryEntry(db.EmbeddedDocument):
     filename = db.StringField(required=True)
-    date = db.DateTimeField(required=True)
+    date = db.DateTimeField(required=True,default=datetime.utcnow)
