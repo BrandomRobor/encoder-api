a9a180e935dc6e1241ccd0b4bcded794379b645a
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-22 20:34:50 -0700

    Loading Mongo host from environment variable

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ from .db import initialize_db
 from flask import Flask
 
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {
+    "host": app.config.from_envvar("MONGODB_HOST")
+}
 initialize_db(app)
 
 if __name__ == "__main__":
