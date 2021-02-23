d6bdd483c1b7dbb10d575b47db6258fcedf7d8c8
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-23 14:33:54 -0700

    Rearranged code to remove redundancy

--- src/routes/history.py
+++ src/routes/history.py
@@ -7,6 +7,7 @@ from bson import ObjectId
 
 history = Blueprint(__name__, __name__)
 
+
 @history.route("/api/history", methods=["GET"])
 @jwt_required()
 def get_history():
@@ -21,21 +22,18 @@ def get_history():
 @history.route("/api/history", methods=["PUT"])
 @jwt_required()
 def add_to_history():
-    body = request.get_json()
-
     try:
         user_history = UserHistoryEntry.objects.get(user_id=get_jwt_identity())
     except DoesNotExist:
-        new_user_history = UserHistoryEntry()
-        new_user_history.user_id = get_jwt_identity()
-        new_user_history.history.append(HistoryEntry(**body))
-        new_user_history.save()
-        return Response(status=201)
+        user_history = UserHistoryEntry()
+        user_history.user_id = get_jwt_identity()
+    except:
+        return Response(status=500)
+
+    try:
+        user_history.history.append(HistoryEntry(**request.get_json()))
+        user_history.save()
+    except:
+        return Response(status=500)
     else:
-        try:
-            user_history.history.append(HistoryEntry(**body))
-            user_history.save()
-        except:
-            return Response(status=500)
-        else:
-            return Response(status=201)
+        return Response(status=201)
