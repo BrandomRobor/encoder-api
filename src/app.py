6321b03daa058f5d045303c86564f490d2986b9c
Brandom Rodriguez <brandomrobor@tuta.io>
2021-02-22 21:16:03 -0700

    Get config from environments variable instead of external file

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 from db import initialize_db
 from flask import Flask
+import os
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {
-    "host": app.config.from_envvar("MONGODB_HOST")
-}
+app.config["MONGODB_HOST"] = os.environ.get("MONGODB_HOST")
 initialize_db(app)
 
 if __name__ == "__main__":
